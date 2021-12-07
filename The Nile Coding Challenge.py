8bdce35ac8b063cacbd5bbfa0a93b8bffd93b431
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-12-07 11:43:39 -0500

    calculate shipping and test

--- The Nile Coding Challenge.py
+++ The Nile Coding Challenge.py
@@ -2,3 +2,13 @@ from nile import get_distance, format_price, SHIPPING_PRICES
 from test import test_function
 
 
+# Define calculate_shipping_cost() here:
+def calculate_shipping_cost(from_coords, to_coords, shipping_type = 'Overnight'):
+  distance = get_distance(*from_coords, *to_coords)
+  shipping_rate = SHIPPING_PRICES[shipping_type]
+  price = distance * shipping_rate
+  return format_price(price)
+
+
+# Test the function by calling 
+test_function(calculate_shipping_cost)
